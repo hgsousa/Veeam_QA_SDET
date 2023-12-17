1e3def54c5c72c310f572639335e9005fec229db
Hugo <75086815+hgsousa@users.noreply.github.com>
2023-12-17 17:35:31 +0000

    QA.py

--- QA.py
+++ QA.py
@@ -5,22 +5,6 @@ import argparse
 import hashlib
 import shutil
 
-"""
-A program that synchronizes two folders: source and replica. The program should maintain a full, identical copy of source folder at replica folder.
-
-1) Synchronization must be one-way: after the synchronization content of the replica folder 
-should be modified to exactly match content of the sourcefolder;  (-->DONE<--)
-
-2) Synchronization should be performed periodically. (-->DONE<--)
-
-3) File creation/copying/removal operations should be logged to a file and to the console output; (-->DONE<--)
-
-4) Folder paths, synchronization interval and log file path should be provided using the command line arguments; (-->DONE<--)
-
-5) It is undesirable to use third-party libraries that implement folder synchronization;  (-->DONE<--)
-
-6) It is allowed (and recommended) to use external libraries implementing other well-known algorithms. (-->DONE<--)
-"""
 
 
 def verify_folders_satus(source_folder, replica_folder):
